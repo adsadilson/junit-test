a662615d661b697facfd751ab6777fb89dd688e3
adsadilson <adilson.curso@yahoo.com.br>
2022-08-31 23:54:16 -0300

    Configuration do profile develop

---
+++ src/main/java/br/com/apss/junittest/core/config/LocalConfig.java
@@ -0,0 +1,25 @@
+package br.com.apss.junittest.core.config;
+
+import br.com.apss.junittest.domain.model.User;
+import br.com.apss.junittest.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+   @Autowired
+   private UserRepository userRepository;
+
+   @Bean
+   public void startDB(){
+      User user1 = User.builder().name("adsadilson").email("adsdilson@yahoo.com.br").password("254").build();
+      User user2 = User.builder().name("Renato").email("renato@yahoo.com.br").password("58se254").build();
+      userRepository.saveAll(List.of(user1,user2));
+   }
+}
