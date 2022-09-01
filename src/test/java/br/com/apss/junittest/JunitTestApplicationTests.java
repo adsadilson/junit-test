3fb8462c8d60cc2556d6d49e18c317f189b897b0
adsadilson <adilson.curso@yahoo.com.br>
2022-08-31 22:11:57 -0300

    Start project junit-test

---
+++ src/test/java/br/com/apss/junittest/JunitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apss.junittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
